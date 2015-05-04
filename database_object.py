66f0051b21d0d23b912a4e6fd52a18582c6e6586
richarla <richarla@onid.oregonstate.edu>
2015-05-04 13:13:37 -0700

    Create database_object.py

---
+++ database_object.py
@@ -0,0 +1,11 @@
+import webapp2
+import MySQLdb
+
+
+class DatabaseObject:
+	def __init__(self):
+		self.db = MySQLdb.connect(unix_socket='/cloudsql/recycling-app-13:storage', db='app_storage', user='root')
+		self.cursor = self.db.cursor()
+
+	def __del__(self):
+		self.db.close()
